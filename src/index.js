246340be65706335be33a3d6787257ba9c711449
vivek <vivekranjan8789@gmail.com>
2024-05-23 22:44:48 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ const app = express();
 const PORT = 3000;
 
 const connect = require('./config/database');
-const Tweet = require('./models/tweet');
-
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+const { TweetRepository } = require('./repository/tweet-repository');
+const  TweetService  = require('./services/tweet-service');
 
 app.listen(PORT, async() => {
         console.log(`Server started at port ${PORT}`);
         await connect();
         console.log("mongoDB connected");
-
-        const tweets =await Tweet.find({
-                content : ['updated data', 'First Tweet']
-        });
-        console.log(tweets);
         
 })
 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,22 +1,34 @@
-const TweetRepository = require('../repository/index');
+const { TweetRepository, HashtagRepository } = require('../repository/index');
 
 class TweetService {
     constructor(){
-        this.TweetService = new TweetRepository()
+        this.TweetRepository = new TweetRepository();
+        this.HashtagRepository = new HashtagRepository();
     }  
 
     async create(data) {
-        const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extract hashtags 
-        tags = tags.map((tag) => tag.substring(1));
-        console.log(tags);
-        const tweet = await this.create(data);
-        // todo create hashtags and add here
-        /***
-         * 1. bulcreate in mongoose
-         * 2. filter title of hashtag based on multiple tags
-         * 3. how to add id inside the hashtags
-         */
+        const content = data.content; // content of data is taken
+        let tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extract hashtags 
+        tags = tags.map((tag) => tag.substring(1));    // removed the '#' from all hashtags in from starting
+        const tweet = await this.TweetRepository.create(data);  // tweet created but i have to create hashtag also if not present in the db
+
+        let alreadyPresentTags = await this.HashtagRepository.findByName(tags) // will give whole object in which tags are already present
+        console.log("already present tags are: ", alreadyPresentTags);
+        let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);  // will give an array of already present tag
+          
+        let newTags = tags.filter(tag => !titleOfPresenttags.includes(tag)); // will give array of tags that are not present in db
+        // converting the array of hashtags to object so that we can create the new hashtags
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]};
+        })
+
+         await this.HashtagRepository.bulkCreate(newTags);  // new hashtags are being created
+        // adding tweets to already present hashtags 
+        alreadyPresentTags.forEach((tag) => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
+  
         return tweet;
     }
 }
