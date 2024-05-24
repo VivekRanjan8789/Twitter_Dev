25dd2516c0d688a836897018eec02abdcb8bc726
vivek <vivekranjan8789@gmail.com>
2024-05-24 18:24:57 +0530

    Added crud repo and extends it in likerepo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,50 +1,37 @@
 import Tweet from '../models/tweet.js'
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
-    async create(data) {
-        try {
-            const tweet = Tweet.create(data);
-            return tweet;            
-        } catch (error) {
-            console.log(error);
+class TweetRepository extends  CrudRepository {
+      constructor() {
+        super(Tweet);
+      }
+        async create(data) {
+            try {
+                const tweet = Tweet.create(data);
+                return tweet;            
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        async getWithComments(id){
+            try {
+                const tweet = Tweet.findById(id).populate({path: 'comments'}).lean();
+                return tweet;            
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+
+        async getAll(offset, limit){
+            try {
+                const tweet = await Tweet.find().skip(offset).limit(limit);
+                return tweet;            
+            } catch (error) {
+            console.log(error);          
+            }
         }
     }
-
-    async get(id) {
-        try {
-            const tweet = Tweet.findById(id);
-            return tweet;            
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async getWithComments(id){
-        try {
-            const tweet = Tweet.findById(id).populate({path: 'comments'}).lean();
-            return tweet;            
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async destroy(id) {
-        try {
-            const tweet = Tweet.findByIdAndDelete(id);
-            return tweet;            
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async getAll(offset, limit){
-        try {
-            const tweet = await Tweet.find().skip(offset).limit(limit);
-            return tweet;            
-        } catch (error) {
-         console.log(error);          
-        }
-    }
-}
 
 export default TweetRepository;
