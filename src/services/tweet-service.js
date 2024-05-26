0c99a10e408e0ce2466fb04e8c1e064e7f1f5243
vivek <vivekranjan8789@gmail.com>
2024-05-27 00:11:52 +0530

    Fixed error if hashtag not present in tweet

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -7,26 +7,29 @@ class TweetService {
 
     async create(data) {
         const content = data.content; // content of data is taken
-        let tags = content.match(/#[a-zA-Z0-9_]+/g) // this regex extract hashtags 
-                    .map((tag) => tag.substring(1))    // removed the '#' from all hashtags in from starting
-                    .map(tag => tag.toLowerCase());    // in db-> we will store our hashtags only in lowerCase
         const tweet = await this.TweetRepository.create(data);  // tweet created but i have to create hashtag also if not present in the db
+        let tags = content.match(/#[a-zA-Z0-9_]+/g) // this regex extract hashtags 
+            if(tags){
+                tags.map((tag) => tag.substring(1))    // removed the '#' from all hashtags in from starting
+                .map(tag => tag.toLowerCase());    // in db-> we will store our hashtags only in lowerCase
+            
 
-        let alreadyPresentTags = await this.HashtagRepository.findByName(tags) // will give whole object in which tags are already present
-        let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);  // will give an array of already present tag
-          
-        let newTags = tags.filter(tag => !titleOfPresenttags.includes(tag)); // will give array of tags that are not present in db
-        // converting the array of hashtags to object so that we can create the new hashtags
-        newTags = newTags.map(tag => {
-            return {title: tag, tweets: [tweet.id]};
-        })
+                let alreadyPresentTags = await this.HashtagRepository.findByName(tags) // will give whole object in which tags are already present
+                let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);  // will give an array of already present tag
+                
+                let newTags = tags.filter(tag => !titleOfPresenttags.includes(tag)); // will give array of tags that are not present in db
+                // converting the array of hashtags to object so that we can create the new hashtags
+                newTags = newTags.map(tag => {
+                    return {title: tag, tweets: [tweet.id]};
+                })
 
-         await this.HashtagRepository.bulkCreate(newTags);  // new hashtags are being created
-        // adding tweets to already present hashtags 
-        alreadyPresentTags.forEach((tag) => {
-            tag.tweets.push(tweet.id);
-            tag.save();
-        });
+                await this.HashtagRepository.bulkCreate(newTags);  // new hashtags are being created
+                // adding tweets to already present hashtags 
+                alreadyPresentTags.forEach((tag) => {
+                    tag.tweets.push(tweet.id);
+                    tag.save();
+                });
+            }
   
         return tweet;
     }
